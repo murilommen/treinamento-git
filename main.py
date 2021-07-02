dfae13805c9797f5e223e4c187717d00d712093a
Murilo Menezes Mendonça <murilommen@gmail.com>
2021-07-02 15:55:25 -0300

    adding main file execution

---
+++ main.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+# MAGIC %run ./feature_engineering
+
+# COMMAND ----------
+
+# MAGIC %run ./train
+
+# COMMAND ----------
+
+# MAGIC %run ./predict
+
+# COMMAND ----------
+
+# MAGIC %run ./validate
+
+# COMMAND ----------
+
+# MAGIC %run ./serving
